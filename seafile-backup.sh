ce6e85739447f2cf665384b2e7b804b22eb26d71
joshtyler <josh.tyler@btinternet.com>
2017-11-23 23:01:09 +0000

    Add email

--- seafile-backup.sh
+++ seafile-backup.sh
@@ -7,9 +7,6 @@
 # Website: 3c7.me
 ###############################
 
-# Usage:
-# ./seafile-backup.sh SEAFDIR SEAFDATADIR TEMPDIR 
-
 # Variables
 DATE=`date +%F`
 TIME=`date +%H%M`
@@ -17,48 +14,52 @@ SEAFDIR=/home/josh/seafile
 SEAFDATADIR=/media/server-files/seafile
 TEMPDIR=/media/server-files/seafile-backup
 
-# Shutdown seafile
-echo Shutting down seafile
-$SEAFDIR/seafile-server-latest/seahub.sh stop
-$SEAFDIR/seafile-server-latest/seafile.sh stop
+{
+	echo "Subject: Seafile backup status"
+	# Shutdown seafile
+	echo Shutting down seafile
+	$SEAFDIR/seafile-server-latest/seahub.sh stop
+	$SEAFDIR/seafile-server-latest/seafile.sh stop
 
-# Create directories
-if [ ! -d $TEMPDIR ]
-  then
-  echo Create temporary directory $TEMPDIR
-  mkdir -pm 0600 $TEMPDIR
-  mkdir -m 0600 $TEMPDIR/databases
-  mkdir -m 0600 $TEMPDIR/data
-fi
+	# Create directories
+	if [ ! -d $TEMPDIR ]
+	  then
+	  echo Create temporary directory $TEMPDIR
+	  mkdir -pm 0600 $TEMPDIR
+	  mkdir -m 0600 $TEMPDIR/databases
+	  mkdir -m 0600 $TEMPDIR/data
+	fi
 
-# Dump data / copy data
-echo Dumping GroupMgr database...
-sqlite3 $SEAFDIR/ccnet/GroupMgr/groupmgr.db .dump > $TEMPDIR/databases/groupmgr.db.bak
-if [ -e $TEMPDIR/databases/groupmgr.db.bak ]; then echo ok.; else echo ERROR.; fi
-echo Dumping UserMgr database...
-sqlite3 $SEAFDIR/ccnet/PeerMgr/usermgr.db .dump > $TEMPDIR/databases/usermgr.db.bak
-if [ -e $TEMPDIR/databases/usermgr.db.bak ]; then echo ok.; else echo ERROR.; fi
-echo Dumping SeaFile database...
-sqlite3 $SEAFDIR/seafile-data/seafile.db .dump > $TEMPDIR/databases/seafile.db.bak
-if [ -e $TEMPDIR/databases/seafile.db.bak ]; then echo ok.; else echo ERROR.; fi
-echo Dumping SeaHub database...
-sqlite3 $SEAFDIR/seahub.db .dump > $TEMPDIR/databases/seahub.db.bak
-if [ -e $TEMPDIR/databases/seahub.db.bak ]; then echo ok.; else echo ERROR.; fi
+	# Dump data / copy data
+	echo Dumping GroupMgr database
+	sqlite3 $SEAFDIR/ccnet/GroupMgr/groupmgr.db .dump > $TEMPDIR/databases/groupmgr.db.bak
+	if [ -e $TEMPDIR/databases/groupmgr.db.bak ]; then echo ok.; else echo ERROR.; fi
+	echo Dumping UserMgr database...
+	sqlite3 $SEAFDIR/ccnet/PeerMgr/usermgr.db .dump > $TEMPDIR/databases/usermgr.db.bak
+	if [ -e $TEMPDIR/databases/usermgr.db.bak ]; then echo ok.; else echo ERROR.; fi
+	echo Dumping SeaFile database...
+	sqlite3 $SEAFDIR/seafile-data/seafile.db .dump > $TEMPDIR/databases/seafile.db.bak
+	if [ -e $TEMPDIR/databases/seafile.db.bak ]; then echo ok.; else echo ERROR.; fi
+	echo Dumping SeaHub database...
+	sqlite3 $SEAFDIR/seahub.db .dump > $TEMPDIR/databases/seahub.db.bak
+	if [ -e $TEMPDIR/databases/seahub.db.bak ]; then echo ok.; else echo ERROR.; fi
 
-echo Copying seafile directory
-rsync -az $SEAFDATADIR/* $TEMPDIR/data
-if [ -d $TEMPDIR/data/seafile-data ]; then echo ok.; else echo ERROR.; fi
+	echo Copying seafile directory
+	rsync -az $SEAFDATADIR/* $TEMPDIR/data
+	if [ -d $TEMPDIR/data/seafile-data ]; then echo ok.; else echo ERROR.; fi
 
-# Start the server
-echo Restarting seafile
-$SEAFDIR/seafile-server-latest/seafile.sh start
-$SEAFDIR/seafile-server-latest/seahub.sh start-fastcgi
+	# Start the server
+	echo Restarting seafile
+	$SEAFDIR/seafile-server-latest/seafile.sh start
+	$SEAFDIR/seafile-server-latest/seahub.sh start-fastcgi
 
-# Send data to backblaze
-echo Syncing to backblaze
-b2 sync --delete --keepDays 5 $TEMPDIR b2://joshtyler-seafile-backup/
+	# Send data to backblaze
+	echo Syncing to backblaze
+	b2 sync --delete --keepDays 5 $TEMPDIR b2://joshtyler-seafile-backup/
 
-# Cleanup
-echo Deleting temporary files...
-rm -Rf $TEMPDIR
-if [ ! -d $TEMPDIR ]; then echo ok.; else echo ERROR.; fi
+	# Cleanup
+	echo Deleting temporary files...
+	rm -Rf $TEMPDIR
+	if [ ! -d $TEMPDIR ]; then echo ok.; else echo ERROR.; fi
+
+} | tee | sendmail -f josh.tyler@btinternet.com josh.tyler@btinternet.com
